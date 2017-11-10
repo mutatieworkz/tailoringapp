46c8e3962b6de5a2be62bebabd20d1836e83b496
Ponnusamy <131240@NTTDATA.COM>
2017-11-10 19:12:47 +0530

    added Search functionality in customer list page

--- src/pages/customer-list/customer.ts
+++ src/pages/customer-list/customer.ts
@@ -8,9 +8,15 @@ import { CustomersDetailsPage } from '../customersDetails/customersDetails';
     templateUrl: 'customer.html',
 })
 export class CustomerPage {
+    customers: Array<any> = [];
     MaleCustomers: Array<any> = [];
     FemaleCustomers: Array<any> = [];
     gender: string = "Male";
+
+
+    toggled: boolean;
+    searchTerm: String = '';
+
 
     @ViewChild(Content) content: Content;
 
@@ -30,20 +36,48 @@ export class CustomerPage {
 
     ionViewDidEnter() {
         this.content.resize();
-      }
+    }
 
     loadCustomers() {
         this.databaseProvider.getAllCustomers().then(data => {
-            this.MaleCustomers = [];
-            this.FemaleCustomers = [];
-            this.MaleCustomers = data.filter(gender => gender.Gender == "Male");
-            this.FemaleCustomers = data.filter(gender => gender.Gender == "Female");
+            this.customers = data;
+            this.FilterCustomer(data);
         });
+    }
+
+    private FilterCustomer(data: any[]) {
+        this.MaleCustomers = [];
+        this.FemaleCustomers = [];
+        this.MaleCustomers = data.filter(gender => gender.Gender == "Male");
+        this.FemaleCustomers = data.filter(gender => gender.Gender == "Female");
     }
 
     itemTapped(event, customer) {
         this.navCtrl.push(CustomersDetailsPage, {
             customer: customer
         });
+    }
+
+    toggleSearch() {
+        this.toggled = this.toggled ? false : true;
+        this.searchTerm = null;
+    }
+
+    searchCancel() {
+        this.FilterCustomer(this.customers);
+        this.toggleSearch();
+    }
+
+    triggerInput(ev: any) {
+        // set val to the value of the searchbar
+        let val = ev.target.value;
+        // if the value is an empty string don't filter the items
+        if (val && val.trim() != '') {
+            this.MaleCustomers = this.customers.filter(gender => gender.Gender == "Male" && (gender.Name.toLowerCase().indexOf(val.toLowerCase()) > -1 || gender.Phone.indexOf(val) > -1));
+            this.FemaleCustomers = this.customers.filter(gender => gender.Gender == "Female" && (gender.Name.toLowerCase().indexOf(val.toLowerCase()) > -1 || gender.Phone.indexOf(val) > -1));
+        }
+        else {
+            this.FilterCustomer(this.customers);
+        }
     }
 }
